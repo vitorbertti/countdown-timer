ac55879731c90308454a8d4e0d7fbdfd4ec2fcf5
Vitor Bertti <berttivitor@gmail.com>
2021-03-07 13:00:33 -0300

    Added countdown function

---
+++ script.js
@@ -0,0 +1,26 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const finalDate = '25 Dec 2021';
+
+function countdown() {
+   const currentDate = new Date();
+   const newFinalDate = new Date(finalDate);
+
+   const totalSeconds = (newFinalDate - currentDate) / 1000;
+   const days = Math.floor(totalSeconds / 3600 / 24);
+   const hours = Math.floor(totalSeconds / 3600) % 24;
+   const minutes = Math.floor(totalSeconds / 60) % 60;
+   const seconds = Math.floor(totalSeconds) % 60;
+
+   daysEl.innerHTML = days;
+   hoursEl.innerHTML = hours;
+   minutesEl.innerHTML = minutes;
+   secondsEl.innerHTML = seconds;
+}
+
+countdown();
+
+setInterval(countdown, 1000);
